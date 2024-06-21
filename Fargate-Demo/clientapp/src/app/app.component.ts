ff627cd389ce5892d4aa484b6e2ff5528c8acbc9
Paras Patil <paras.patil@aircanada.ca>
2024-06-21 22:02:49 +0530

    added dist

--- Fargate-Demo/clientapp/src/app/app.component.ts
+++ Fargate-Demo/clientapp/src/app/app.component.ts
@@ -1,10 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'clientapp';
+  response: string = '';
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+    this.httpClient.get('WeatherForecast').subscribe((res) => {
+      console.log(res);
+      this.response = JSON.stringify(res);
+    });
+  }
 }
